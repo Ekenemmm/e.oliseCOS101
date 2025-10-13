6f6bc53bfe195178d01c9e9ce7357bc5e2fcba85
ekenemmm <ekenem.olise@pau.edu.ng>
2025-10-13 19:03:42 +0100

    (week3commit)

---
+++ week-3/practice_1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\EKENEM\Documents\e.oliseCOS101\week-3\practice_1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\EKENEM\Documents\e.oliseCOS101\week-3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week-3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\EKENEM\Documents\e.oliseCOS101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\EKENEM\Documents\e.oliseCOS101\week-3\practice_8\src\main.rs
