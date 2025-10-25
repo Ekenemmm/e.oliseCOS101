c7959d9b9165b60133653dbeb0478000b64a84bf
ekenemmm <ekenem.olise@pau.edu.ng>
2025-10-25 20:32:35 +0100

    revisited

--- week-4/project-3/target/debug/deps/project_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\EKENEM\Documents\e.oliseCOS101\week-4\project-3\target\debug\deps\project_3.d: src\main.rs
-
-C:\Users\EKENEM\Documents\e.oliseCOS101\week-4\project-3\target\debug\deps\project_3.exe: src\main.rs
-
-src\main.rs:
--- week-4/project-3/target/debug/project-3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\EKENEM\Documents\e.oliseCOS101\week-4\project-3\target\debug\project-3.exe: C:\Users\EKENEM\Documents\e.oliseCOS101\week-4\project-3\src\main.rs
