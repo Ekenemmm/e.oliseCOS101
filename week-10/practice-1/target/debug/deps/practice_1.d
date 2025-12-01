fb7364e58557e4e5e51b8f286e574dd128a49fc4
ekenemmm <ekenem.olise@pau.edu.ng>
2025-12-01 11:30:57 +0100

    (week10)

---
+++ week-10/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\EKENEM\Documents\e.oliseCOS101\week-10\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\EKENEM\Documents\e.oliseCOS101\week-10\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice-4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\EKENEM\Documents\e.oliseCOS101\week-10\practice-4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\EKENEM\Documents\e.oliseCOS101\week-10\practice-4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
